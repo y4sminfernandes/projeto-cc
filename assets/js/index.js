f53b324f7644d99df31ead0c602c7534e15f8dff
Yasmin Fernandes <130566782+y4sminfernandes@users.noreply.github.com>
2025-05-15 21:56:17 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,11 @@
-const port = 3000;
+const { Pool } = require('pg');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
 
 const pool = new Pool({
   connectionString: 'postgresql://postgres:Coolmida1234@db.zswimtruiwtwrqxadmnl.supabase.co:5432/postgres',
